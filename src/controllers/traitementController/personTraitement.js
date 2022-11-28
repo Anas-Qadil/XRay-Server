7afa2b5c7b49623e12dab589115c934dc2476676
Anas Qadil <qadilanass10@gmail.com>
2022-11-28 13:30:35 +0100

    update

--- src/controllers/traitementController/personTraitement.js
+++ src/controllers/traitementController/personTraitement.js
@@ -27,46 +27,59 @@ const addPersonTraitement = async (req, res) => {
           validTraitementData.push(doc);
         }
       });
-      await savedTraitement.populate("person");
+      // await savedTraitement.populate("person");
       await savedTraitement.populate("service");
       await savedTraitement.populate("service.hospital");
-      await savedTraitement.populate("person.company");
-
+      await savedTraitement.populate({
+        path: "person",
+        populate: {
+          path: "company",
+        }
+      });
+      // Promise.all([savedTraitement]).then((result) => {
+      //   console.log(result);
+      // });
       let totalDoses = 0;
       validTraitementData.map((doc) => {
         totalDoses += Number(doc.dose);
       });
 
-      if (parseInt(totalDoses) >= 18) {
-        const email = savedTraitement?.person?.email;
-        if (email) {
-          if (validator.isEmail(email))
-            sendEmail(email);
-        }
-        const hospitalEmail = savedTraitement?.service?.hospital?.email;
-        if (hospitalEmail) {
-          if (validator.isEmail(hospitalEmail))
-          {
-            sendAdminMail(hospitalEmail, savedTraitement?.person?.cin);
+      Promise.resolve(savedTraitement).then((result) => {
+        if (parseInt(totalDoses) >= 18) {
+          const email = result?.person?.email;
+          if (email) {
+            if (validator.isEmail(email))
+              sendEmail(email);
+          }
+          const hospitalEmail = result?.service?.hospital?.email;
+          console.log(hospitalEmail);
+          if (hospitalEmail) {
+            if (validator.isEmail(hospitalEmail))
+            {
+              sendAdminMail(hospitalEmail, result?.person?.cin);
+            }
+          }
+        
+          const companyEmail = result?.person?.company?.email;
+          console.log(companyEmail);
+          if (companyEmail) {
+            if (validator.isEmail(companyEmail))
+              sendAdminMail(companyEmail, result?.person?.cin);
+          }
+  
+          phone = result?.person?.phone;
+          if (phone) {
+            sendSMS(phone);
           }
         }
+      });
       
-        const companyEmail = savedTraitement?.person?.company?.email;
-        if (companyEmail) {
-          if (validator.isEmail(companyEmail))
-            sendAdminMail(companyEmail, savedTraitement?.person?.cin);
-        }
-
-        phone = savedTraitement?.person?.phone;
-        if (phone) {
-          sendSMS(phone);
-        }
-        const adminEmail = await adminModel.findOne({}).select("email");
-        if (adminEmail) {
-          if (validator.isEmail(adminEmail?.email))
-            sendAdminMail(adminEmail?.email, savedTraitement?.person?.cin);
-        }
+      const adminEmail = await adminModel.findOne({}).select("email");
+      if (adminEmail) {
+        if (validator.isEmail(adminEmail?.email))
+          sendAdminMail(adminEmail?.email, savedTraitement?.person?.cin);
       }
+
       res.status(200).send({ 
       message: "Traitement saved successfully",
       traitement: savedTraitement
@@ -78,6 +91,7 @@ const addPersonTraitement = async (req, res) => {
     })
   }
 }
+
 
 const getPersonTraitements = async (req, res) => {
   try {
